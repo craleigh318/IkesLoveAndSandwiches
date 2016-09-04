cceec62f4576a64ac34f45888820005f2e52ec53
Christopher Raleigh <craleigh318@comcast.net>
2016-09-03 19:21:23 -0700

    Created XCode project.

---
+++ Ike's Love and Sandwiches/Ike's Love and Sandwiches/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ike's Love and Sandwiches
+//
+//  Created by Christopher Raleigh on 2016-09-03.
+//  Copyright © 2016 Ike's Place. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
