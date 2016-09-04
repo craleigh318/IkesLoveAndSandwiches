e6dfc44cfb232818b2c035a66dc7da4568f2b67d
Christopher Raleigh <craleigh318@comcast.net>
2016-09-04 14:52:10 -0700

    Added add-on class.

---
+++ Ike's Love and Sandwiches/Ike's Love and Sandwiches/PFoodItem.swift
@@ -0,0 +1,12 @@
+//
+//  PFoodItem.swift
+//  Ike's Love and Sandwiches
+//
+//  Created by Christopher Raleigh on 2016-09-04.
+//  Copyright © 2016 Ike's Place. All rights reserved.
+//
+
+protocol PFoodItem {
+    var name: String {get}
+    var price: Int {get}
+}
---
+++ Ike's Love and Sandwiches/Ike's Love and Sandwiches/Sandwich.swift
@@ -0,0 +1,22 @@
+//
+//  Sandwich.swift
+//  Ike's Love and Sandwiches
+//
+//  Created by Christopher Raleigh on 2016-09-04.
+//  Copyright © 2016 Ike's Place. All rights reserved.
+//
+
+class Sandwich: PFoodItem {
+    var name: String {
+        return NSLocalizedString(internalName, comment: "")
+    }
+    
+    private(set) var price: Int
+    
+    private var internalName: String
+    
+    init(internalName: String, price: Int = 0) {
+        self.internalName = internalName
+        self.price = price
+    }
+}
