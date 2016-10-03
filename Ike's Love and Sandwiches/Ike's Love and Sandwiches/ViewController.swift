df0abc32277445d396da72be7246be6971304b33
Christopher Raleigh <craleigh318@comcast.net>
2016-10-03 00:02:52 -0700

    First working version.

--- Ike's Love and Sandwiches/Ike's Love and Sandwiches/ViewController.swift
+++ Ike's Love and Sandwiches/Ike's Love and Sandwiches/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: SandwichCustomizerView {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let sandwich = SandwichesMenu.meatSandwiches[2]
+        let customizer = SandwichCustomizer(baseSandwich: sandwich)
+        setCustomizer(customizer: customizer)
     }
 
     override func didReceiveMemoryWarning() {
